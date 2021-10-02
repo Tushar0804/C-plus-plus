d1bf510681ae939b143d1b8a04305a6fcc5485a4
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-02 12:52:04 +0530

    Update Practical_04_Task_03.cpp

--- Practical_04/Practical_04_Task_03.cpp
+++ Practical_04/Practical_04_Task_03.cpp
@@ -20,9 +20,9 @@ namespace second
 
 int main()
 {
-	cout << "Sum = " << add(2, 3) << endl;
-	cout << "Sum = " << add(2.2f, 3) << endl;			// Error Ambiguity
-	cout << "Sum = " << add(0, 10.5f)<< endl;			// Error Ambiguity
+	cout << "Sum = " << add(2, 3) << endl;				// add function not found.
+	cout << "Sum = " << add(2.2f, 3) << endl;			
+	cout << "Sum = " << add(0, 10.5f)<< endl;			
 	cout << "Sum = " << add(5.5f, 4.1f)<< endl;
 	return 0;
 }
