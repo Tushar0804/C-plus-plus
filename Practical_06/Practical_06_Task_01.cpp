761b3ae1d3880cf1f956a877c5ebd2b7dc06a8f0
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-07 21:38:59 +0530

    Create Practical_06_Task_01.cpp

---
+++ Practical_06/Practical_06_Task_01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 5;
+    float c = 50.5f, d = 40.6f;
+    string e = "Hello";
+    string f = "World";
+    
+	cout << "int + int = " << a + b << endl;
+	
+	cout << "float + float = " << c +  d << endl;
+	
+	cout << "int + float = " << a + c << endl;
+    
+  cout << "string + string = " << e + f << endl;
+    
+  cout << "string + int = " << e + a << endl;     // ERROR!
+    
+	return 0;
+}
