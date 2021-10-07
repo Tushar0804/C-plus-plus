cc34148e711eff8cbd8e5251b7afe8669352a0ca
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-07 21:10:30 +0530

    Create Practical_05_Task_01.cpp

---
+++ Practical_05/Practical_05_Task_01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch = 'A';
+	
+	int a = 1456000;
+	short b = 95;
+	long c = 123456789;
+	
+	float d = 10.568f;
+	double e = 15556.5556669;
+	long double f = 102568955.4644335L;
+	
+	bool g = true;
+	
+	cout << "Character ch = " << ch << endl;
+	cout << "Integer a = " << a << endl;
+	cout << "Short Integer b = " << b << endl;
+	cout << "Long Integer c = " << c << endl;
+	cout << "Floating Point d = " << d << endl;
+	cout << "Double e = " << e << endl;
+	cout << "Long Double f = " << f << endl;
+	cout << "Boolean g = " << g << endl;
+	
+	return 0;
+}
