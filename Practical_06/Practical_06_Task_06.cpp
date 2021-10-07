871953f6e4cbe4cd3a1590b5083a83731f8cba74
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-07 23:31:02 +0530

    Add files via upload

---
+++ Practical_06/Practical_06_Task_06.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+	s = "Hello";
+	cout << "String s is : " << s << endl;
+	
+	s[0] = 'J';			// strings are mutable in c++.
+
+	cout << "String s is : " << s << endl;
+	return 0;
+}
