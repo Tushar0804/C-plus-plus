4d71ef72387015d47a800308acac1cb2f450faf1
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-17 19:39:29 +0530

    Add files via upload

---
+++ Practical_07/Practical_07_Task_03_V2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{	
+	int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	
+	cout << "Array is : " << endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}	
+	
+	cout << "\nArray after reverse : " << endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << 10 - arr[i][j] << " ";
+		cout << endl;
+	}	        
+	return 0;
+}
