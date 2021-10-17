d3e18285b4c818dcf0c78ca273da120089b89b01
Tushar Sharma <91238367+Tushar0804@users.noreply.github.com>
2021-10-17 21:05:57 +0530

    Add files via upload

---
+++ Practical_07/Practical_07_Task_08.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	char arr[4][4];	
+	
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			if(i+j < 4)
+				arr[i][j] = '*';
+			else
+				arr[i][j] = ' ';
+		}				
+	}	
+	
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}		        
+	return 0;
+}
